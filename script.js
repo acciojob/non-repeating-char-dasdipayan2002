9b29678387a6b9a8d0d66dfad2df28bf2824ee75
Dipayan Das <105163490+dasdipayan2002@users.noreply.github.com>
2024-04-19 11:29:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    let charCount = {};
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (charCount[char] == 1) {
+            return char;
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
